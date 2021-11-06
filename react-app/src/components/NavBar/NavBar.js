b1bdd94b752bf2bd7bb895a612d10bccc0e31370
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-06 14:07:46 -0400

    navbar done

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navBar.css';
-import Logo from '../../public/Logo.svg'
+
 
 
 const NavBar = () => {
   return (
     <nav>
-      <img alt="Logo" src={Logo}/>
-
+      <div >
+        <NavLink to="/">
+      <img alt="Logo" className="logo" src="https://res.cloudinary.com/dxo7djnid/image/upload/v1636220158/ignored/WHITE_Chat_Communication_Logo_1500_x_500_px_190_x_40_px_1_etunj2.svg"/>
+      </NavLink>
+      </div>
         <button className="loginBtn">
           <NavLink to='/login' exact={true} activeClassName='active' >
             Login

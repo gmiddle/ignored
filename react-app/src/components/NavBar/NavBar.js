c6fcf66493f21fb85eb930d8b704ff7a1666b414
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-06 13:52:53 -0400

    logo up

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navBar.css';
+import Logo from '../../public/Logo.svg'
 
 
 const NavBar = () => {
   return (
     <nav>
+      <img alt="Logo" src={Logo}/>
 
         <button className="loginBtn">
           <NavLink to='/login' exact={true} activeClassName='active' >
--- react-app/src/components/Splash/Index.js
+++ react-app/src/components/Splash/Index.js
@@ -1,13 +1,16 @@
 import './Splash.css'
-
+import { NavLink } from 'react-router-dom';
 const Splash = () => {
     return (
         <>
 
 
         <div className="mainDiv">
-        <h1 className = "splashMainHeader"> Ignored? stay in touch. </h1>
-        <h2 className ="splashSubHeader"> A site that makes it easy to talk every day and hang out more often.</h2>
+        <h1 className = "splashMainHeader"> IGNORED? STAY IN TOUCH </h1>
+        <h2 className ="splashSubHeader"> A SITE THAT MAKES IT EASY TO TALK EVERY DAY AND HANG OUT MORE OFTEN</h2>
+        <button className="splashButton">
+        <NavLink to="/login">Open Ignored</NavLink>
+        </button>
 
         </div>
 

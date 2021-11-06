d9b16509ce4d8caaf75b3eff70f8e782342d98b8
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-06 17:46:43 -0400

    login button hidden when logged in

--- react-app/src/components/NavBar/NavBar.js
+++ react-app/src/components/NavBar/NavBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './navBar.css';
+import {useSelector} from 'react-redux'
 
 
 
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user);
+
   return (
     <nav>
       <div >
@@ -12,11 +15,12 @@ const NavBar = () => {
       <img alt="Logo" className="logo" src="https://res.cloudinary.com/dxo7djnid/image/upload/v1636220158/ignored/WHITE_Chat_Communication_Logo_1500_x_500_px_190_x_40_px_1_etunj2.svg"/>
       </NavLink>
       </div>
-        <button className="loginBtn">
+     {!sessionUser?  <button className="loginBtn">
           <NavLink to='/login' exact={true} activeClassName='active' >
             Login
           </NavLink>
-        </button>
+        </button>: null}
+
 
     </nav>
   );

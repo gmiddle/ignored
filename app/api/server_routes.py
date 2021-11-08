a322231d86be1adf8f39939835968aef5c8a42a0
noah <garciasharrettsnoah@gmail.com>
2021-11-08 15:02:19 -0800

    almost done with server routes

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, Flask, redirect, request
 from flask_login import login_required, current_user
 from app.forms import ServerForm
 from app.models import Server, PrivateServer, db
+from werkzeug.security import generate_password_hash
 
 server_routes = Blueprint('servers', __name__)
 private_server_routes = Blueprint('private_servers', __name__)
@@ -30,36 +31,35 @@ def private_server(id):
     return private_server.to_dict()
 
 # POST a server
-@server_routes.route('/', mehtods=['POST'])
+@server_routes.route('/', methods=['POST'])
 def servers_post():
   """
   Creates a new server
   """
   form = ServerForm()
 
-  if request.method == 'POST':
-
-    if form.validate_on_submit():
-      server = Server(
-        name=form.data['Name'],
-        description=form.data['Description'],
-        serverImg=form.data['ServerImg']
-      )
-      db.session.add(server)
-      db.seesion.commit()
-      return redirect('/')
-    else:
-      print(form.errors)
-      return "Bad data"
+  if form.validate_on_submit():
+    server = Server(
+      name=form.data['Name'],
+      description=form.data['Description'],
+      serverImg=form.data['ServerImg'],
+      serverInviteKey = generate_password_hash(f"{form.data['Name']}")[-7:-1].upper()
+    )
+    db.session.add(server)
+    db.seesion.commit()
+    return redirect('/')
+  else:
+    print(form.errors)
+    return "Bad data"
 
       
 # POST a private_server
-@private_server_routes.route('/', methods=['POST'])
-def private_server_post():
-  pass
+# @private_server_routes.route('/', methods=['POST'])
+# def private_server_post():
+#   pass
 
 # PUT edit server
-@server_routes.routes('/edit/<init: id>', methods=['PUT'])
+@server_routes.route('/edit/<int:id>', methods=['PUT'])
 def servers_edit(id):
   server_edit = Server.query.get_or_404(id)
   form = ServerForm()
@@ -67,7 +67,7 @@ def servers_edit(id):
     server_edit.name = form.data['Name'],
     server_edit.description = form.data['Description'],
     server_edit.serverImg = form.data['ServerImg']
-  try:
+  try: 
     db.session.commit()
     return redirect('/')
   except:
@@ -75,7 +75,7 @@ def servers_edit(id):
     return "Bad data"
 
 
-@server_routes.routes('/delete/<int: id>', methods=['DELETE'])
+@server_routes.route('/delete/<int:id>', methods=['DELETE'])
 def server_delete(id):
   server = Server.query.get_or_404(id)
   try:

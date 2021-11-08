6af25893866afd2a4dd77c4f624db717a649dbc2
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-08 18:22:45 -0500

    dashboard wip

--- react-app/src/components/Chat/Index.js
+++ react-app/src/components/Chat/Index.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import './Chat.css'
+import ChatHeader from '../ChatHeader/Index'
 
 const Chat = () => {
     return (
         <div className='chat'>
-            
+            <ChatHeader />
+            <div className='chatMessages'>
+                </div>
 
+                <div className='chatArea'>
+                    <form>
+                        <input type='text' placeholder={`Message #ChannelName `} />
+                        <button type='submit'className="chatSubmitBtn">
+                            Send Message
+                        </button>
+                        </form>
+
+                </div>
         </div>
     )
 }

d464b7613e9f6637b35ce9ff6faea133feeb2b59
Andrewcodes12 <AndrewFava@yahoo.com>
2021-11-10 15:39:44 -0500

    merged with main

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -82,9 +82,9 @@ def servers_edit(id):
 @server_routes.route('/delete/<int:id>', methods=['DELETE'])
 def server_delete(id):
   server = Server.query.filter(Server.id == id).first()
-
   try:
     db.session.delete(server)
     db.session.commit()
     return "Server Deleted"
-
+  except:
+    return "No Server Found"
